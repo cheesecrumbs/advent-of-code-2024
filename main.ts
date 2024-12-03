2f507583eda2b2746df581957e7cedbde06f78f9
Markus <Mar.Jae@web.de>
2024-12-03 20:30:25 +0100

    Solve Day 3 Bonus

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 import { day1_1, day1_2 } from "./src/day1.ts";
 import { day2_1, day2_2 } from "./src/day2.ts";
-import { day3_1 } from "./src/day3.ts";
+import { day3_1, day3_2 } from "./src/day3.ts";
 
 // Day 1
-// console.log(await day1_1());
-// console.log(await day1_2());
+console.log(await day1_1());
+console.log(await day1_2());
 
 // Day 2
-// console.log(await day2_1());
-// console.log(await day2_2());
+console.log(await day2_1());
+console.log(await day2_2());
 
 // Day 3
 console.log(await day3_1());
+console.log(await day3_2());
--- src/day3.ts
+++ src/day3.ts
@@ -5,9 +5,30 @@ export async function day3_1() {
     const text = await Deno.readTextFile(FILE_PATH);
     const mulRegex = /mul\(\d*,\d*\)/gm;
 
-    return text.match(mulRegex)?.map((match) =>
-        match.replace("mul(", "").replace(")", "").split(",").map((val) =>
-            Number(val)
-        ).reduce((a, b) => a * b)
-    ).reduce((a, b) => a + b);
+    return mapMatchesToResult(text.match(mulRegex), mulRegex);
+}
+
+export async function day3_2() {
+    const text = await Deno.readTextFile(FILE_PATH);
+    const mulRegex = /mul\(\d*,\d*\)/gm;
+    const doAndDontMulRegex = /(mul\(\d*,\d*\))|don't\(\)(.|\n)+?(do\(\))/gm;
+
+    const validMatches = text.match(doAndDontMulRegex)?.filter((match) =>
+        !match.startsWith("don't()")
+    ).flatMap((validMatch) => validMatch);
+
+    return mapMatchesToResult(validMatches, mulRegex);
+}
+
+function mapMatchesToResult(
+    matches: RegExpExecArray | string[] | null | undefined,
+    regExp: RegExp,
+): number | undefined {
+    return matches?.flatMap((match) => match.match(regExp))
+        .filter((match) => match !== null)
+        .map((match) =>
+            match.replace("mul(", "").replace(")", "").split(",").map((val) =>
+                Number(val)
+            ).reduce((a, b) => a * b)
+        ).reduce((a, b) => a + b);
 }
